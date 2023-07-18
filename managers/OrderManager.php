b55662269e5d80fee1ac1284fc8c4db3210cd02d
3lvir4 <pierreavril18@gmail.com>
2023-07-18 10:58:45 +0200

    init OrderManager -> add method getOrderById + started method getOrdersByUser_id

---
+++ managers/OrderManager.php
@@ -0,0 +1,30 @@
+<?php
+
+class OrderManager extends AbstractManager {
+	public function getOrderById(int $id) : Order {
+		$query = $this->db->prepare('
+			SELECT * FROM orders WHERE id = :id
+		');
+		$parameters = [
+			'id' => $id
+		];
+		$query->execute($parameters);
+		$result = $query->fetch(PDO::FETCH_ASSOC);
+		$order = Order::createInstanceFromAssoc($result);
+		return $order;
+	}
+	public function getOrdersByUser_id(int $user_id) : array {
+		$query = $this->db->prepare('
+			SELECT * FROM orders WHERE user_id = :user_id
+		');
+		$parameters = [
+			'user_id' => $user_id
+		];
+		$query->execute($parameters);
+		$results = $query->fetchAll(PDO::FETCH_ASSOC);
+		$orders = [];
+		foreach($results as $assoc) {
+			
+		}
+	}	
+}
